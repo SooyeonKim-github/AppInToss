b32930793b69d22a81482185a6acda5641d8dbe5
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-07 22:09:57 +0900

    feat: add chart interpretation package

---
+++ question_generator/interpretation/__init__.py
@@ -0,0 +1,3 @@
+from .chart_explainer import build_intro_tip, build_result_explanation, feature_counts, select_primary_features
+
+__all__ = ["build_intro_tip", "build_result_explanation", "feature_counts", "select_primary_features"]
