4c0c5ba9a941d9aa99868dece51440d8e0f130bf
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-07 22:27:58 +0900

    refactor: expose Indicators-based feature engine

--- question_generator/features/__init__.py
+++ question_generator/features/__init__.py
@@ -1,3 +1,10 @@
-from .chart_feature_engine import FEATURE_EXTRACTORS, FeatureSignal, extract_feature_signals
+from .chart_feature_engine import FeatureSignal
+from .indicator_feature_engine import EXTRACTORS as FEATURE_EXTRACTORS
+from .indicator_feature_engine import add_indicator_snapshot, extract_feature_signals
 
-__all__ = ["FeatureSignal", "FEATURE_EXTRACTORS", "extract_feature_signals"]
+__all__ = [
+    "FeatureSignal",
+    "FEATURE_EXTRACTORS",
+    "add_indicator_snapshot",
+    "extract_feature_signals",
+]
