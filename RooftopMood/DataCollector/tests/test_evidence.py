7af86fdd19bb2290c1d9fbe43de21d40309fc618
sooyeonkim96 <80865733+SooyeonKim-github@users.noreply.github.com>
2026-09-08 22:05:57 +0900

    test: update evidence relevance for Kakao blog V2

--- RooftopMood/DataCollector/tests/test_evidence.py
+++ RooftopMood/DataCollector/tests/test_evidence.py
@@ -1,11 +1,13 @@
 from pipeline.evidence_collection import EvidenceCollectionPipeline
 
 
-def test_relevance_score_prefers_cafe_and_keyword():
+def test_relevance_score_prefers_cafe_region_keyword_and_tistory():
     score = EvidenceCollectionPipeline._relevance_score(
         "무드카페",
-        "무드카페 루프탑",
-        "무드카페 루프탑 후기",
+        "성수",
+        "ROOFTOP",
+        "무드카페 성수 루프탑 후기",
         "서울에서 노을이 예쁜 루프탑 카페",
+        True,
     )
     assert score >= 90
